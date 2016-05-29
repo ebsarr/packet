7d78a4c21c13b4304070be6429520b2cfb6c2202
bassirou-sarr <tsub1208>
2016-05-29 19:02:22 +0900

    Added documentation

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # packet, a CLI tool to manage packet services
+
+packet is a CLI tool to manage packet services. You can browse the documetation [here](doc/packet.md).
+
+**NOTE**
---
+++ cmd/gendoc.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/spf13/cobra/doc"
+)
+
+// gendocCmd represents the gendoc command
+var gendocCmd = &cobra.Command{
+	Use:   "gendoc",
+	Short: "Generate a markdown documentation file for this tool",
+	Hidden:	true,
+	Run: func(cmd *cobra.Command, args []string) {
+		doc.GenMarkdownTree(RootCmd, "./doc")
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(gendocCmd)
+}
